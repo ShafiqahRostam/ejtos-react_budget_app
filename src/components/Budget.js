a8b7e6cb7f2e37ebc2e5f10f96038e5eed6f358d
Shafiqah <shafiqah.rostam@hotmail.sg>
2024-05-10 17:39:06 +0800

    Add components and functionalities based on project instructions

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,21 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+    const handleBudgetChange = (event) => {
+        setNewBudget(event.target.value);
+        console.log('set new budget', event.target.value);
+    }
+
+    return (
+        <div className='alert alert-secondary'>
+            {/* <span>Budget: &#163;{budget}</span> */}
+            <span>Budget: &#163;</span>
+            <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+        </div>
+    );
+}
+
+export default Budget;
